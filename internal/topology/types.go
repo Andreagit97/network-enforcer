1bd829d9a775795d279fcc6a9d24a6a373f7a3a4
Andrea Terzolo <andrea.terzolo@suse.com>
2026-08-14 16:23:14 +0200

    refactor: remove unused types
    Signed-off-by: Andrea Terzolo <andrea.terzolo@suse.com>

--- internal/topology/types.go
+++ internal/topology/types.go
@@ -1,10 +1,6 @@
 package topology
 
 import (
-	"k8s.io/apimachinery/pkg/util/sets"
-
-	corev1 "k8s.io/api/core/v1"
-
 	"github.com/rancher-sandbox/network-enforcer/internal/ownerkind"
 )
 
@@ -12,32 +8,4 @@ type WorkloadKey struct {
 	Namespace string
 	OwnerKind ownerkind.Kind
 	OwnerName string
-}
-
-type FlowRecord struct {
-	Source     WorkloadKey
-	Dest       WorkloadKey
-	DstPort    int32
-	Protocol   corev1.Protocol
-	SrcAddress string
-	DstAddress string
-}
-
-type FlowKey struct {
-	Source   WorkloadKey
-	Dest     WorkloadKey
-	DstPort  int32
-	Protocol corev1.Protocol
-}
-
-type Peer struct {
-	WorkloadKey
-
-	DstPort  int32
-	Protocol corev1.Protocol
-}
-
-type WorkloadConnections struct {
-	Egress  map[WorkloadKey]sets.Set[Peer]
-	Ingress map[WorkloadKey]sets.Set[Peer]
 }
